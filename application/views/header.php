159014b8e7e02a0856ec765301faae729f269c05
rohit13d <rohit@13d.com>
2016-05-25 15:59:37 +0530

    upload utility code
    recent code

---
+++ application/views/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <script src="/codeignitor/upload/assets/js/jquery-2.1.1.min.js"></script>
+ 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.1/js/bootstrap-datepicker.js"></script>
+  <script src="/codeignitor/upload/assets/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="/codeignitor/upload/assets/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.1/css/bootstrap-datepicker.css">
+<script src="/codeignitor/upload/assets/js/function.js"></script>
+  <style type="text/css">
+     .colbox {
+          margin-left: 0px;
+          margin-right: 0px;
+     }
+	 body {
+    background-image: url('/codeignitor/upload/assets/dryleaves.jpg');
+}
+.well
+{
+margin-top: 115px;	
+background-color : #58D68D; 
+margin-left: -17px;
+ 
+}
+     </style>
+</head>
+<body>
+
+
+<?php
+
---
+++ application/views/upload_articles.php
@@ -0,0 +1,23 @@
+
+<div class="container">
+		<?php 
+          echo form_open("home/upload_articles");?>
+    <div class="row" style="background:#ccffcc;padding-top:9px" id="art1">
+		<div name="art" class="row">
+        <div class='col-sm-6' id="articlelables">
+		<label for="title">Article Title</label><br><br>
+		<label for="name">Article Name</label>
+		</div>
+		<div class='col-sm-6' id="articlefields">
+		<input type="text" name="article_title" id="t1"><br><br>
+		<input type="text" name="article_name" id="t2"><br><br>
+		</div>
+		</div>
+		</div>
+		<div class='col-sm-4' id="articlelables">
+		<input id="addmorebtn" name="btn_login" type="button" class="btn btn-default" value="Add More" onclick="add_div()"/>
+		</div>
+		<?php echo form_close(); ?>
+   
+</div>
+ 
